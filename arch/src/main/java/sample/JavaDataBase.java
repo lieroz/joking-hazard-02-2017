535a5e86ced2ae32e0b9880d1788f7a4e5c73f3a
wolf1996 <ksgiv37@gmail.com>
2017-03-03 01:00:02 +0300

    all

--- arch/src/main/java/sample/JavaDataBase.java
+++
@@ -1,6 +0,0 @@
-package sample;
-
-@SuppressWarnings("ALL")
-public class JavaDataBase {
-
-}
---
+++ src/main/java/sample/UserData.java
@@ -0,0 +1,7 @@
+package sample;
+
+/**
+ * Created by ksg on 03.03.17.
+ */
+public class UserData {
+}
