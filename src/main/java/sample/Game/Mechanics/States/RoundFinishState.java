3335ff3b69512745d422ecd223a715e0907af773
wolf1996 <ksgiv37@gmail.com>
2017-05-25 02:25:59 +0300

    recommit

--- src/main/java/sample/Game/Mechanics/States/RoundFinishState.java
+++ src/main/java/sample/Game/Mechanics/States/RoundFinishState.java
@@ -5,9 +5,13 @@ import sample.Game.Mechanics.GameUser.GameUserItem;
 import sample.Game.Mechanics.MainMechanics;
 import sample.Game.Messages.BaseGameMessage;
 import sample.Game.Messages.BaseMessageContainer;
+import sample.Game.Messages.ServerMessages.BaseServerMessage;
+import sample.Game.Messages.ServerMessages.TableInfo;
+import sample.Game.Messages.ServerMessages.UsersCardsInfo;
 import sample.Game.Messages.UserMessages.ChooseCardFromTable;
 import sample.Game.Mechanics.GameContext;
 
+import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.Map;
 import java.util.Objects;
@@ -24,11 +28,14 @@ public class RoundFinishState extends GameState {
     @Override
     public GameState.ErrorCodes transfer() {
         this.context.state = this;
-        context.master.getCardFromTable();
+        BaseServerMessage msg = new UsersCardsInfo(context.mapper,
+                new ArrayList<GameCard>( context.table.values()));
         for (Map.Entry<String, GameUserItem> entry : context.mp.entrySet()) {
             final GameUserItem user = entry.getValue();
+            user.sendMessage(msg);
             user.resetMessage();
         }
+        context.master.getCardFromTable();
         return GameState.ErrorCodes.OK;
     }
 
