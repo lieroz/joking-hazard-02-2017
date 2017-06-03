3fcc874b71d32f0b8bfab8b5d74fe8201cbeb716
wolf1996 <ksgiv37@gmail.com>
2017-06-03 11:44:38 +0300

    fixe user exit3

--- src/main/java/sample/Game/Messages/UserMessages/UserExit.java
+++ src/main/java/sample/Game/Messages/UserMessages/UserExit.java
@@ -1,11 +1,15 @@
 package sample.Game.Messages.UserMessages;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
 import sample.Game.Messages.BaseGameMessage;
 
 /**
  * Created by ksg on 02.06.17.
  */
 public class UserExit extends BaseGameMessage {
+    @JsonCreator
+    public UserExit(){}
+    @Override
     public String getType() {
         return "UserExited";
     }
